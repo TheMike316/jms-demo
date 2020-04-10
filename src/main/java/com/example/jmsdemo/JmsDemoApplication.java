19c3f4f8e511b9286e042c4df742bf16960cc285
mike <hornegger@view-elevator.com>
2020-04-10 19:52:58 +0200

    use dockerized activemq

--- src/main/java/com/example/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/example/jmsdemo/JmsDemoApplication.java
@@ -1,25 +1,12 @@
 package com.example.jmsdemo;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsDemoApplication {
 
-    public static void main(String[] args) throws Exception {
-
-        // set up embedded activemq server (obviously only for demo stuff)
-        var config = new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setSecurityEnabled(false)
-                .setJournalDirectory("build/data/journal")
-                .addAcceptorConfiguration("invm", "vm://0");
-
-        ActiveMQServers.newActiveMQServer(config).start();
-
+    public static void main(String[] args) {
         SpringApplication.run(JmsDemoApplication.class, args);
     }
-
 }
